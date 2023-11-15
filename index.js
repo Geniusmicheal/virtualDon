add3240a3b219512a6b73f5d7a982aa6da7a4b30
Genius Micheal <oluwasegunmicheal10@gmail.com>
2023-11-15 10:25:15 +0100

    application setup

---
+++ index.js
@@ -0,0 +1,12 @@
+function render(el) {
+
+}
+
+function h(type, props, ...children) {}
+
+function view(count) { //@
+	return <ul id="cool" className="foo">
+		<li className="test">text 1 </li>
+		<li>text 2 </li>
+	</ul>
+}
